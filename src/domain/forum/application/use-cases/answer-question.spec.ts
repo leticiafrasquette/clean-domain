664a44aba5db505f6efffeff30058df9382c22b7
Letícia Frasquette <frasquetteleticia@gmail.com>
2024-03-19 22:03:13 -0300

    Continuing the study of DDD and Clean Architecture

---
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -0,0 +1,24 @@
+import { expect, it } from 'vitest'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { AnswerQuestionUseCase } from './answer-question'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase
+
+describe('Create Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
+  })
+
+  it('it should be able to create an answer', async () => {
+    const { answer } = await sut.execute({
+      questionId: '1',
+      instructorId: '1',
+      content: 'Conteúdo da Resposta',
+    })
+
+    expect(answer.id).toBeTruthy()
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+  })
+})
---
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -0,0 +1,33 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { Answer } from '../../enterprise/entities/answer'
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+interface AnswerQuestionUseCaseResponse {
+  answer: Answer
+}
+
+export class AnswerQuestionUseCase {
+  constructor(private answerRepository: AnswersRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
+
+    await this.answerRepository.create(answer)
+
+    return { answer }
+  }
+}
